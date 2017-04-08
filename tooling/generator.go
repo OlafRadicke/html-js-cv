b2ac2e0626d733b326cf2f393af20b5f7e340ea8
Olaf Radicke <briefkasten@olaf-radicke.de>
2017-04-08 19:38:14 +0200

    small fixes.

--- tooling/generator.go
+++ tooling/generator.go
@@ -50,31 +50,20 @@ type PersVita struct {
 
 func main() {
     var persVita = readConfig()
-
-    fmt.Printf("read template...\n")
     t := template.New("html-cv.tmpl")
     t.ParseFiles("html-cv.tmpl")
-
-    fmt.Printf("persVita value: %v\n", persVita)
-    fmt.Printf("execute template...\n")
     err := t.Execute(os.Stdout, persVita)
     if err != nil {
         panic(err)
     }
-
 }
 
 func readConfig()(persVita  PersVita){
-    fmt.Printf("readConfig...\n")
-    // var persVita = PersVita{}
     file, e := ioutil.ReadFile("./vita.json")
     if e != nil {
       fmt.Printf("File error: %v\n", e)
       os.Exit(1)
     }
-
-    fmt.Printf("file value: %v\n", string(file))
     json.Unmarshal(file, &persVita)
-    fmt.Printf("persVita.Years[0].Date: %v\n", persVita.Years[0].Date)
     return persVita
 }
